0b7219219c887a37b99914bfc11c1893e959e715
script01 <christianonyango92@gmail.com>
2019-08-05 16:55:50 +0300

    created an api for phone number to mfl verification

--- .history/index_20190802051504.js
+++ .history/index_20190802051504.js
@@ -6,15 +6,13 @@ require("dotenv").config();
 const users = require("./routes/users");
 const clients = require("./routes/clients");
 const app_diary = require("./routes/appointment_diary");
-const verify = require("./routes/processes/verify_mflcode");
 
 app.use(express.json());
 app.use("/users", users);
 app.use("/clients", clients);
 app.use("/receiver", app_diary);
-app.use("/verifyMFLCode", verify);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
-    console.log(`Ushauri Web App started. Listening on Port: ${PORT}`)
+  console.log(`Ushauri Web App started. Listening on Port: ${PORT}`)
 );
