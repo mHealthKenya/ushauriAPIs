1dc567649dddb2ae10840dbe32f226320c8120fc
script01 <christianonyango92@gmail.com>
2019-08-06 12:45:04 +0300

    pull request

---
+++ .history/models/clinic_20190806115533.js
@@ -0,0 +1,4 @@
+const sequelize = require("../db_config");
+const Sequelize = require("sequelize");
+
+const Clinic
---
+++ .history/routes/processes/clinic_movement_20190806123823.js
@@ -0,0 +1,3 @@
+const { Client } = require("../../models/client");
+const { Clinic } = require("../../models/clinic");
+const base64 = require("base64util");
