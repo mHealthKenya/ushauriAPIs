07eac61a98877b4e8a20fe0d1f9ca357f5da3f39
Ronnie Oduor <oduor.ron@gmail.com>
2019-09-16 12:16:41 +0300

    added sende to be used by mpunda

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ const app_diary = require("./routes/appointment_diary");
 const verify = require("./routes/processes/verify_mflcode");
 const todaysAppointments = require("./routes/processes/process_today_appointment");
 const pastAppointments = require("./routes/processes/process_past_appointment");
+const sender = require("./routes/processes/sender");
 
 // app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/clients", clients);
+app.use("/sender", sender);
 app.use("/receiver", app_diary);
 app.use("/verifyMFLCode", verify);
 app.use("/today_appointments", todaysAppointments);
@@ -23,5 +25,5 @@ app.use("/past_appointments", pastAppointments);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
-    console.log(`Ushauri Web App started. Listening on Port: ${PORT}`)
+  console.log(`Ushauri Web App started. Listening on Port: ${PORT}`)
 );
