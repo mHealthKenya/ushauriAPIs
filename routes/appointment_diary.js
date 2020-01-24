5791e1e47010651ab88b01f07bff7e4f72cafb88
stuxnet01 <christianonyango92@gmail.com>
2020-01-24 10:43:30 +0300

    pull request

--- routes/appointment_diary.js
+++ routes/appointment_diary.js
@@ -110,7 +110,6 @@ router.get("/:id", async(req, res) => {
                 if (Array.isArray(result)) {
                     for (let i = 0; i < result.length; i++) {
                         let msg = "<# " + result[i] + "afS/RY5iPcB>";
-                        // console.log(`todays: ${msg}`);
                         Sender(phone, msg);
                     }
                     Sender(phone, result);
@@ -120,7 +119,6 @@ router.get("/:id", async(req, res) => {
                 if (Array.isArray(pastresult)) {
                     for (let i = 0; i < pastresult.length; i++) {
                         let msg = "<# " + pastresult[i] + "afS/RY5iPcB>";
-                        // console.log(`past: ${msg}`);
                         Sender(phone, msg);
                     }
                 }
